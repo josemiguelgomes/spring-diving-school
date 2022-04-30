38ecde1d79b5bbb32fa4d762f4423d8246c44f6d
Jose Miguel Gomes <jose_miguel_gomes@hotmail.com>
2022-04-30 11:31:17 +0100

    Generic for DTO. closes #67

--- diving-school-web/src/main/java/com/zem/diveschool/dto/GenericDto.java
+++ diving-school-web/src/main/java/com/zem/diveschool/dto/GenericDto.java
@@ -1,9 +1,4 @@
 package com.zem.diveschool.dto;
 
-import org.jetbrains.annotations.NotNull;
-
-public abstract class GenericDto<T> implements Comparable<T> {
-    public int compareTo(@NotNull T o) {
-        return 0;
-    }
+public abstract class GenericDto<T> {
 }
