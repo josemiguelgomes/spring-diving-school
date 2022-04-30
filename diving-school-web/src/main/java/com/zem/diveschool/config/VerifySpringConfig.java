bf04f43f0ed36a54dc219bddddb4530a45b1e84e
Jose Miguel Gomes <jose_miguel_gomes@hotmail.com>
2022-04-30 22:50:23 +0100

    CircleCI

--- diving-school-web/src/main/java/com/zem/diveschool/config/VerifySpringConfig.java
+++ diving-school-web/src/main/java/com/zem/diveschool/config/VerifySpringConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan({ "com.zem.dive.school.persistence.service" })
 public class VerifySpringConfig {
 
     @Bean(name="imAliveService")
